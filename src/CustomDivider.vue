d65646711b0606e855049a88d2e98d61abdeaa35
pablitodon <pavelkazakevich04011996@gmail.com>
2025-01-02 22:55:08 +0600

    vue_first

---
+++ src/CustomDivider.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="divider" :style="dividerStyle"></div>
+  </template>
+
+<script>
+
+
+export default {
+  name: 'CustomDivider',
+  props: {
+    thickness: {
+      type: String,
+      default: '1px'  // Толщина по умолчанию
+    },
+    color: {
+      type: String,
+      default: '#000' // Цвет по умолчанию
+    },
+    styleType: {
+      type: String,
+      default: 'solid' // Стиль по умолчанию
+    },
+    margin: {
+      type: String,
+      default: '0' // Отступы по умолчанию
+    }
+  },
+  computed: {
+    dividerStyle() {
+      return {
+        borderTop: `${this.thickness} ${this.styleType} ${this.color}`,
+        margin: this.margin,
+        width: '100%', // Ширина разделителя
+      };
+    }
+  }
+}
+</script>
+<style scoped>
+.divider {
+  width: 100%; /* Разделитель занимает всю ширину контейнера */
+}
+</style>
---
+++ src/TestComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item, idx) in items" :key="idx">
+                {{ item.price }}
+            </li>
+        </ul>
+        <button @click="addAndUpdate">Add and update</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                { name: 'Artem', price: 1 },
+                { name: 'Pavel', price: 2 }
+            ]
+        }
+    },
+    methods: {
+        addAndUpdate() {
+            const newItem = { name: 'New', price: '-' };
+            this.items.push(newItem);
+            setTimeout(() => {
+                newItem.price = 'over 9000';
+            }, 3000);
+        }
+    }
+};
+</script>
+<style></style>
